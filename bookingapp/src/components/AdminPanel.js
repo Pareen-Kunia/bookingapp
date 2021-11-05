6854d836523c5cfc89b6d6076b771e9ff5759042
Carson <rodriguescarson@hotmail.com>
2021-11-05 10:54:51 +0530

    add: admin pages

--- bookingapp/src/components/AdminPanel.js
+++ bookingapp/src/components/AdminPanel.js
@@ -3,6 +3,11 @@ import { Admin, Resource } from 'react-admin'
 import restProvider from 'ra-data-simple-rest'
 import { AppointmentList, AppointmentEdit, AppointmentCreate } from './Appointment';
 import { UserList, UserEdit, UserCreate } from './User';
+import { CustomerList, CustomerCreate, CustomerEdit } from './Customer';
+import { CalenderList, CalenderEdit, CalenderCreate } from './Calender';
+import { EmployeeList, EmployeeEdit, EmployeeCreate } from './Employee';
+import { RoleList, RoleEdit, RoleCreate } from './Role';
+
 import { createTheme } from '@material-ui/core/styles';
 
 const theme = createTheme({
@@ -19,6 +24,11 @@ function AdminPanel() {
             <Admin theme={theme} dataProvider={dataProvider}>
                 <Resource name="appointments" list={AppointmentList} create={AppointmentCreate} edit={AppointmentEdit} />
                 <Resource name="users" list={UserList} create={UserCreate} edit={UserEdit} />
+                <Resource name="calenders" list={CalenderList} create={CalenderCreate} edit={CalenderEdit} />
+                <Resource name="customers" list={CustomerList} create={CustomerCreate} edit={CustomerEdit} />
+                <Resource name="employees" list={EmployeeList} create={EmployeeCreate} edit={EmployeeEdit} />
+                <Resource name="roles" list={RoleList} create={RoleCreate} edit={RoleEdit} />
+
             </Admin>
         </>
     );
