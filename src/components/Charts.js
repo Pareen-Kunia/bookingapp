8941f558e69b0d89e90aa7ad49d2a2f63f1bef4e
Siddhesh Kamath <63893110+PSYCHOSIDX@users.noreply.github.com>
2021-11-09 13:34:22 +0530

    .

---
+++ src/components/Charts.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.css';
+import Typography from '@mui/material/Typography';
+const Charts = () => {
+     return (
+          <>
+          <div id="img-container"> <Typography gutterBottom variant="h4" padding="2rem" font-weight="500" color="white" component="div">
+        Analytics Of Services
+        </Typography>
+
+     <img id="chart" src='./images/chart.png'/></div>
+          
+               
+          </>
+     )
+}
+
+export default Charts
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Admin from './Admin';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Admin />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
