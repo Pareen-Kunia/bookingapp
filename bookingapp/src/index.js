9cd1fb67596e9db87f2ecbc214e3dda468d53cec
Carson <rodriguescarson@hotmail.com>
2021-11-09 18:11:26 +0530

    fix: dataprovider working

--- bookingapp/src/index.js
+++ bookingapp/src/index.js
@@ -1,14 +1,15 @@
- import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import SignUp from './SignUp';
 import SignIn from './SignIn';
-import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import AdminPanel from './components/AdminPanel';
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
-    <SignUp />
+    <AdminPanel />
     {/* <SignIn /> */}
   </React.StrictMode>,
   document.getElementById('root')
