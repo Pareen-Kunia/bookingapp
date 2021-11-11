5e12bab3d55ad47869c4190f65e9983ff284e1fc
swati865 <rswati865@gmail.com>
2021-11-11 15:19:13 +0530

    sadded landing page and routing

--- bookingapp/src/components/landing_page/landing.js
+++ bookingapp/src/components/landing_page/landing.js
@@ -1,87 +1,141 @@
-import React from 'react';
-import '../landing_page/landing.css'
-import Reset from '../reset_pass/reset';
-/* eslint no-use-before-define: 0 */  // --> OFF
+import React from "react";
+import "../landing_page/landing.css";
+import Reset from "../reset_pass/reset";
+/* eslint no-use-before-define: 0 */ // --> OFF
 
 const Navbar = () => {
-    return (
-        <div>
+  return (
+    <div>
+      <header className="l-header">
+        <nav className="nav bd-grid">
+          <div>
+            <a href="/" className="nav__logo">
+              'O'ppointment
+            </a>
+          </div>
 
-            <header class="l-header">
-                <nav class="nav bd-grid">
-                    <div>
-                        <a href='/' class="nav__logo">BookingApp</a>
-                    </div>
+          <div className="nav__menu" id="nav-menu">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <a href="/" className="nav__link active">
+                  Home
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="/" className="nav__link">
+                  About
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="/" className="nav__link">
+                  Skills
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="/" className="nav__link">
+                  Work
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="/contact" className="nav__link">
+                  Contact
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="/" className="nav__link">
+                  Sign Up
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="/" className="nav__link">
+                  Log In
+                </a>
+              </li>
+            </ul>
+          </div>
 
-                    <div class="nav__menu" id="nav-menu">
-                        <ul class="nav__list">
-                            <li class="nav__item"><a href='/' class="nav__link active">Home</a></li>
-                            <li class="nav__item"><a href='/' class="nav__link">About</a></li>
-                            <li class="nav__item"><a href='/' class="nav__link">Skills</a></li>
-                            <li class="nav__item"><a href='/' class="nav__link">Work</a></li>
-                            <li class="nav__item"><a href='/contact' class="nav__link">Contact</a></li>
-                        </ul>
-                    </div>
+          <div className="nav__toggle" id="nav-toggle">
+            <i className="bx bx-menu"></i>
+          </div>
+        </nav>
+      </header>
 
-                    <div class="nav__toggle" id="nav-toggle">
-                        <i class='bx bx-menu'></i>
-                    </div>
-                </nav>
-            </header>
+      <main className="l-main">
+        <div className="home bd-grid" id="home">
+          <div className="home__data">
+            <h1 className="home__title">
+              Hi,
+              <br />
+              I'am <span className="home__title-color">'O'ppointment</span>
+              <br />
+              Your Assistant
+            </h1>
 
-            <main class="l-main">
+            <a href="/" className="button">
+              Contact
+            </a>
+          </div>
 
-            <div class="home bd-grid" id="home">
-                <div class="home__data">
-                    <h1 class="home__title">Hi,<br/>I'am <span class="home__title-color">Booking App</span><br/>Your Assistant</h1>
+          <div className="home__social">
+            <a href="/" className="home__social-icon">
+              <i className="bx bxl-linkedin"></i>
+            </a>
+            <a href="/" className="home__social-icon">
+              <i className="bx bxl-behance"></i>
+            </a>
+            <a href="/" className="home__social-icon">
+              <i className="bx bxl-github"></i>
+            </a>
+          </div>
 
-                    <a href='/' class="button">Contact</a>
-                </div>
+          <div className="home__img">
+            <img src="assets/img/perfil.png" alt="" />
+          </div>
+        </div>
 
-                <div class="home__social">
-                    <a href="/" class="home__social-icon"><i class='bx bxl-linkedin'></i></a>
-                    <a href="/" class="home__social-icon"><i class='bx bxl-behance' ></i></a>
-                    <a href="/" class="home__social-icon"><i class='bx bxl-github' ></i></a>
-                </div>
+        <section className="about section " id="about">
+          <h2 className="section-title">About</h2>
 
-                <div class="home__img">    
-                    <img src="assets/img/perfil.png" alt=""/>
-                </div>
-            </div>
-
-            <section class="about section " id="about">
-                <h2 class="section-title">About</h2>
-
-                <div class="about__container bd-grid">
-                    <div class="about__img">
-                        <img src="assets/img/about.jpg" alt=""/>
+          <div className="about__container bd-grid">
+            <div className="about__img">
+                        <img src="../../assets/oppointment.jpg" alt=""/>
                     </div>
                     
-                    <div>
-                        {/* <h2 class="about__subtitle">I'am Marlon</h2> */}
-                        <p class="about__text">This booking app will help you book appointments for your customer with ease and efficiently.</p>           
-                    </div>                                   
-                </div>
-            </section>
+            <div>
+              {/* <h2 className="about__subtitle">I'am Marlon</h2> */}
+              <p className="about__text">
+                This booking app will help you book appointments for your
+                customer with ease and efficiently.
+              </p>
+            </div>
+          </div>
+        </section>
 
-            <section class="contact section" id="contact">
-                <h2 class="section-title">Contact</h2>
+        <section className="contact section" id="contact">
+          <h2 className="section-title">Contact</h2>
 
-                <div class="contact__container bd-grid">
-                    <form action="" class="contact__form">
-                        <input type="text" placeholder="Name" class="contact__input"/>
-                        <input type="mail" placeholder="Email" class="contact__input"/>
-                        <textarea name="" id="" cols="0" rows="10" class="contact__input"></textarea>
-                        <input type="button" value="Enter" class="contact__button button"/>
-                    </form>
-                </div>
-            </section>
-
-            </main>
-
-
-        </div>
-    );
+          <div className="contact__container bd-grid">
+            <form action="" className="contact__form">
+              <input type="text" placeholder="Name" className="contact__input" />
+              <input type="mail" placeholder="Email" className="contact__input" />
+              <textarea
+                name=""
+                id=""
+                cols="0"
+                rows="10"
+                className="contact__input"
+              ></textarea>
+              <input
+                type="button"
+                value="Enter"
+                className="contact__button button"
+              />
+            </form>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default Navbar;
