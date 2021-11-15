1db650a6d3d9863b5135985eb2c5a50d9e497e33
Siddhesh Kamath <63893110+PSYCHOSIDX@users.noreply.github.com>
2021-11-15 23:55:06 +0530

    added reusable header and minor fixes to admin panel

--- bookingapp/src/components/pannel.js
+++ bookingapp/src/components/pannel.js
@@ -20,8 +20,6 @@ import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
 
 import './style.css';
 import { Button } from '@mui/material';
@@ -32,7 +30,7 @@ import CardEmp from './CardEmp';
 import CardS from './CardS';
 import CardData from './CardData';
 
-
+import MAHeader from './MAHeader';
 import BottomPanel from './BottomPanel';
 import Footer from './Footer/Footer';
 
@@ -57,31 +55,6 @@ const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
   }),
 );
 
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  padding: theme.spacing(0),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-  justifyContent: 'center',
-}));
 
 function Pannel() {
   const theme = useTheme();
@@ -96,93 +69,11 @@ function Pannel() {
   };
 
   return (
+  
     <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="fixed" open={open}>
-        <Toolbar className="nav-bg">
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{ mr: 2, ...(open && { display: 'none' }) }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <nav className="nav">
-            <div className="left">
-              <h4 id="title-admin">MeetInc</h4>
-            </div>
-
-
-            <div className="items">
-              <h3 className="nav-company" > 'O'ppointment</h3>
-              <input className="search" type="text" placeholder="Search.."></input>
-
-              <Button variant="contained" size='small' style={{ borderRadius: "10rem" }}> <SearchIcon color="white" fontSize="medium" /></Button>
-
-            </div>
-
-
-          </nav>
-        </Toolbar>
-
-      </AppBar>
-
-
-
-      <Drawer
-
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="persistent"
-        anchor="left"
-        open={open}
-
-      >
-        <DrawerHeader className="draw-bg">
-          <Typography variant="h4" onClick={handleDrawerClose} > ADMIN PANEL </Typography>
-          <IconButton onClick={handleDrawerClose} >
-            {theme.direction === 'ltr' ? <ChevronLeftIcon className="draw-bg" /> : <ChevronRightIcon />}
-          </IconButton>
-        </DrawerHeader>
-        <Stack direction="row" className="profile">
-
-          <Avatar
-            alt="Siddhesh Kamath"
-            src="./images/sid.jfif"
-
-            sx={{ width: 126, height: 126 }}
-          />
-        </Stack>
-
-        <Typography className="draw-ig" variant="h4" align="center"> Siddhesh Kamath </Typography>
-        <Typography className="para"> Junior Web Developer At Kusol Labs </Typography>
-
-
-        <List className="draw-ig">
-          {['Support', 'Settings'].map((text, index) => (
-            <ListItem className="draw-ig" button key={text}>
-              <ListItemIcon >
-                {index % 3 === 1 ? < FavoriteIcon className="draw-ig" /> : <SettingsIcon className="draw-ig" />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-
-        <card className="draw-ig">
-          {/* <Map className="map"/> */}
-        </card>
-      </Drawer>
-
-
+    
+    <MAHeader />
+    
       <Main className="main" open={open}>
         <div className="card-holder">
           <CardC />
